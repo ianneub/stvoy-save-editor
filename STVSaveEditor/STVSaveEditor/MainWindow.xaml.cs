193c6b6546e2c5617ffdd39ab2c160b717288d87
seanneubert <sean@seanneubert.com>
2026-02-26 12:18:50 -0500

    Updated the windows exe save editor with a new LCARS based UI.
    * Updated the windows exe save editor with a new LCARS based UI. Updated the base resources and crew morale to match whats actually available in game. I also moved all other items into the misc section. These are mostly mission related things that shouldnt be edited unless you know what your doing.
    
    * Fixed code reviewer issue, embeded LCARS font to insure app looks the same on all systems, standardized the min/max/close buttons

--- STVSaveEditor/STVSaveEditor/MainWindow.xaml.cs
+++ STVSaveEditor/STVSaveEditor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Windows;
+using System.Windows.Media;
 using Microsoft.Win32;
 using STVSaveEditor.ViewModels;
 
@@ -6,6 +8,8 @@ namespace STVSaveEditor;
 
 public partial class MainWindow : Window
 {
+    private bool _miscExpanded = false;
+
     public MainWindow()
     {
         InitializeComponent();
@@ -28,4 +32,27 @@ public partial class MainWindow : Window
             vm.SelectedFile = dialog.FileName;
         }
     }
+
+    private void MiscHeader_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        _miscExpanded = !_miscExpanded;
+        MiscItems.Visibility = _miscExpanded ? Visibility.Visible : Visibility.Collapsed;
+        MiscArrow.RenderTransform = _miscExpanded
+            ? new RotateTransform(180, 4, 2)
+            : null;
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2) ToggleMaximize();
+        else DragMove();
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+    
+    private void Maximize_Click(object sender, RoutedEventArgs e) => ToggleMaximize();
+
+    private void Close_Click(object sender, RoutedEventArgs e) => Close();
+
+    private void ToggleMaximize() => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
 }
